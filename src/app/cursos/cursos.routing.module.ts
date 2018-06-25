992b8e254204cbe0c7f8012fd1c7fa73d302cda3
Washington Alexandre da Silva <washington.silva@SPOMM010114173.local>
2018-06-25 08:35:09 -0300

    Rotas: Criando um módulo de rotas de funcionalidade

---
+++ src/app/cursos/cursos.routing.module.ts
@@ -0,0 +1,22 @@
+import { Routes, RouterModule } from '@angular/Router';
+import { NgModule } from '@angular/core';
+
+import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
+import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+
+const cursosRoutes: Routes = [
+  { path: 'cursos', component: CursosComponent},
+  { path: 'curso/:id', component: CursoDetalheComponent },
+  { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(cursosRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class CursosRoutingModule {}
