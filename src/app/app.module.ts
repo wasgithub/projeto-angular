014c46cf7f8e3fb23526390882ff48d738e439f3
Washington Alexandre da Silva <washington.silva@SPOMM010114173.local>
2018-07-05 19:33:25 -0300

    #Rotas: Rotas Filhas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AppRoutingModule } from './app.routing.module';
 import { CursosModule } from './cursos/cursos.module';
+import { AlunosModule } from './alunos/alunos.module';
 // import { MaterializeModule } from 'angular2-materialize';
 
 
@@ -18,6 +19,7 @@ import { CursosModule } from './cursos/cursos.module';
   ],
   imports: [
     BrowserModule,
+    AlunosModule,
     CursosModule,
     AppRoutingModule
     // MaterializeModule
